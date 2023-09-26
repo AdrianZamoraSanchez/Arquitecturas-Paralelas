1cc6c15eed1135bf9be75d01226c79576989d94d
Adrián <74370211+AdrianZamoraSanchez@users.noreply.github.com>
2023-09-26 11:05:45 +0200

    Update MPI-Practica-6.cpp

--- MPI-Practica-6.cpp
+++ MPI-Practica-6.cpp
@@ -1,3 +1,13 @@
+/*
+* Autor: Adrián Zamora Sánchez
+* Fecha: 26/09/2023
+* 
+* Descripción: Programa que cálcula factoriales,
+* para ello un proceso 0 pide los numeros de los cuales
+* calcular el factorial y recibe y muestra los resultados
+* en paralelo un proceso 1 calcula los factoriales.
+*/
+
 #include <mpi.h>
 #include <iostream>
 
@@ -14,56 +24,50 @@ int main(int argc, char* argv[])
     // Crea dos variabes que controlan la comunicación entre procesos
     MPI_Request request_send, request_recv;
 
-    if (mirango == 0) {
-        // Bucle que mantiene funcionando el proceso 0, que pide datos y muestra los resutados
-        while (true) {
-            std::cout << "Proceso " << mirango << ": Introduce un numero para calcular su factorial: ";
-            std::cin >> dato;
+    // Bucle que mantiene funcionando ambos procesos hasta encontrar un 0
+    while (true){
+        // Toma de datos y salida de resultados por el proceso 0
+        if (mirango == 0) {
+                std::cout << "Proceso " << mirango << ": Introduce un numero para calcular su factorial: ";
+                std::cin >> dato;
 
-            // Envía el dato al proceso 1 para su cálculo
-            MPI_Isend(&dato, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request_send);
-            std::cout << "Proceso " << mirango << ": numero(" << dato << ") enviado!\n";
+                // Envía el dato al proceso 1 para su cálculo
+                MPI_Isend(&dato, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request_send);
+                std::cout << "Proceso " << mirango << ": numero(" << dato << ") enviado!\n";
 
-            // Comprueba la condición de salida
-            if (dato == 0) {
-                break; // Salir del bucle si el usuario ingresa 0
-            }
+                // Comprueba la condición de salida
+                if (dato == 0) {
+                    MPI_Finalize();
+                    return 0;
+                }
 
-            std::cout << "Proceso " << mirango << ": recibiendo resultados...\n";
+                std::cout << "Proceso " << mirango << ": recibiendo resultados...\n";
 
-            // Recibe el resultado del proceso 1
-            MPI_Irecv(&resultado, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, &request_recv);
-            // Espera a recibir el resultado
-            MPI_Wait(&request_recv, MPI_STATUS_IGNORE); 
+                // Recibe el resultado del proceso 1
+                MPI_Irecv(&resultado, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, &request_recv);
+                // Espera a recibir el resultado
+                MPI_Wait(&request_recv, MPI_STATUS_IGNORE); 
 
-            // Muestra el resultado
-            std::cout << "Proceso " << mirango << ": el resultado de: " << dato << "! = " << resultado << "\n";
+                // Muestra el resultado
+                std::cout << "Proceso " << mirango << ": el resultado de: " << dato << "! = " << resultado << "\n";
+        }
+        // Cálculos realizados por el proceso 1
+        if (mirango == 1) {
+                // Se espera a recibir el numero del que queremos calcular su factorial
+                MPI_Irecv(&dato, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request_recv);
+                // Espera a recibir el numero
+                MPI_Wait(&request_recv, MPI_STATUS_IGNORE); 
+
+                // Calcula el factorial
+                int factorial = 1;
+                for (int i = 1; i <= dato; ++i) {
+                    factorial *= i;
+                }
+
+                // Envía el resultado al proceso 0
+                MPI_Isend(&factorial, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &request_send);
         }
     }
-    if (mirango == 1) {
-        // Bucle que mantiene funcionando el proceso 1, que recibe el dato, calcula el factorial y devuelve el resultado
-        while (true) {
-            // Se espera a recibir el numero del que queremos calcular su factorial
-            MPI_Irecv(&dato, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request_recv);
-            // Espera a recibir el numero
-            MPI_Wait(&request_recv, MPI_STATUS_IGNORE); 
-
-            // Comprueba la condición de salida
-            if (dato == 0) {
-                break; // Salir del bucle si se recibe 0
-            }
-
-            // Calcula el factorial
-            int factorial = 1;
-            for (int i = 1; i <= dato; ++i) {
-                factorial *= i;
-            }
-
-            // Envía el resultado al proceso 0
-            MPI_Isend(&factorial, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &request_send);
-        }
-    }
-
     // Finaliza el programa
     MPI_Finalize();
     return 0;
